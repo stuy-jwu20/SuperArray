b7b9079853d46bc9ab42d10d8b870eecf2b534bb
Jonathan Wu <jwu20@stuy.edu>
2020-11-02 22:36:12 -0500

    Added a mini tester file

---
+++ Tester.java
@@ -0,0 +1,12 @@
+public class Tester {
+  public static void main(String[] args) {
+    System.out.println("SuperArray Mini-Tester");
+    SuperArray words = new SuperArray();
+    words.add("kani");
+    words.add("uni");
+    words.add("ebi");
+    for(int i = 0; i < words.size(); i++){
+      System.out.println( words.get(i) );
+    }
+  }
+}
