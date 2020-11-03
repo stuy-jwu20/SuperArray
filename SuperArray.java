2417dc1f3d5691ab277e8c5f69d2a7e113241381
Jonathan Wu <jwu20@stuy.edu>
2020-11-02 21:43:34 -0500

    Added add() method

--- SuperArray.java
+++ SuperArray.java
@@ -9,10 +9,15 @@ public class SuperArray {
   public int size() {
     int numberItems = 0;
     for (int i = 0; i < data.length(); i++) {
-      if (data[i] != null) {
-        numberItems++;
-      }
+      if (data[i] != null) numberItems++;
     }
     return numberItems;
+  }
+
+  public boolean add(String element) {
+    for (int i = 0; i < data.length; i++) {
+      if (data[i] == null) data[i] == element return true;
+    }
+    return false;
   }
 }
