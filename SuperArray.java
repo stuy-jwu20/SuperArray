ccc3aea2a0d233bcc46bf8dca52115a5ac136e1a
Jonathan Wu <jwu20@stuy.edu>
2020-11-02 22:33:21 -0500

    Debugged SuperArray.java and should work properly now

--- SuperArray.java
+++ SuperArray.java
@@ -2,51 +2,51 @@ public class SuperArray {
   private String[] data;
   private int size;
 
+  private void resize(){
+    size = size + 10;
+    String[] largerArray = new String [size];
+    for (int i = 0; i < data.length-1; i++) {
+      largerArray[i] = data[i];
+    }
+    data = largerArray;
+  }
+
   public SuperArray() {
     size = 10;
-    this.data = new int [size];
+    this.data = new String [size];
   }
 
   public int size() {
     int numberItems = 0;
-    for (int i = 0; i < data.length(); i++) {
+    for (int i = 0; i < size-1; i++) {
       if (data[i] != null) numberItems++;
     }
     return numberItems;
   }
 
   public boolean add(String element) {
-    for (int i = 0; i < data.length; i++) {
+    for (int i = 0; i < size-1; i++) {
       if (data[i] == null) {
-        data[i] == element
+        data[i] = element;
         return true;
       }
-      else if ()(data[i] != null) && (i == data.length) {
-        data.resize();
-      } else {
-        return false;
+      if ((data[i] != null) && (i == size-2)) {
+        resize();
       }
     }
+    return false;
   }
 
   public String get(int index) {
-    String elementHere = "";
-    elementHere = data[index];
+    String elementHere = "" + data[index];
     return elementHere;
   }
 
   public String set(int index, String element) {
-    String oldValue = "We replaced the value: " + data[index];
-    data[index] == element;
+    String oldValue = data[index];
+    data[index] = element;
     return oldValue;
   }
 
-  private void resize() {
-    size = size * 2;
-    Stringp[] largerArray = new int [size];
-    for (int i = 0; i < data.length; i++) {
-      largerArray[i] == data[i];
-    }
-    data = largerArray;
-  }
+
 }
