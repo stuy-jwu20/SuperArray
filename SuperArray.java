6ad32c247f0227c6506309d441b5b6d3af69ef81
Jonathan Wu <jwu20@stuy.edu>
2020-11-02 21:55:01 -0500

    Modified add() method for resizing

--- SuperArray.java
+++ SuperArray.java
@@ -17,9 +17,16 @@ public class SuperArray {
 
   public boolean add(String element) {
     for (int i = 0; i < data.length; i++) {
-      if (data[i] == null) data[i] == element return true;
+      if (data[i] == null) {
+        data[i] == element
+        return true;
+      }
+      else if ()(data[i] != null) && (i == data.length) {
+        data.resize();
+      } else {
+        return false;
+      }
     }
-    return false;
   }
 
   public String get(int index) {
