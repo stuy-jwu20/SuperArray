bece8efc2bbe527862338bb34db34eb1e436f811
Jonathan Wu <jwu20@stuy.edu>
2020-11-04 21:48:11 -0500

    added some Testing values

--- Tester.java
+++ Tester.java
@@ -1,20 +1,23 @@
 public class Tester {
   public static void main(String[] args) {
     System.out.println("SuperArray Mini-Tester");
-    SuperArray words = new SuperArray();
+    SuperArray words = new SuperArray(0);
     words.add("kani");
     words.add("uni");
     words.add("ebi");
     for(int i = 0; i < words.size(); i++){
       System.out.println( words.get(i) );
     }
-    // System.out.println(words.toString());
-    // System.out.println(words.contains("kani"));
-    // //words.add(1, "cat");
-    // System.out.println(words.contains("kaniUNI"));
-    // System.out.println(words.toString());
-    // System.out.println(words.remove(1));
-    // System.out.println(words.toString());
-    // System.out.println(words.indexOf("kani"));
+    //words.clear();
+    System.out.println(words.toString());
+    System.out.println(words.isEmpty());
+    System.out.println(words.contains("kani"));
+    System.out.println(words.contains("kaniUNI"));
+    words.add(1, "addedString");
+    System.out.println(words.toString());
+    System.out.println(words.remove(0));
+    System.out.println(words.toString());
+    System.out.println(words.indexOf("addedString"));
+    System.out.println(words.toArray().toString());
   }
 }
