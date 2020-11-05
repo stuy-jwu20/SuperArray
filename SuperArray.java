808594a2d6af0ddc9e5e821607e0631c3c836e69
Jonathan Wu <jwu20@stuy.edu>
2020-11-04 21:47:48 -0500

    Debugged SuperArray

--- SuperArray.java
+++ SuperArray.java
@@ -2,7 +2,7 @@ public class SuperArray {
   private String[] data;
   private int size;
 
-  private void resize(){
+  private void resize() {
     if (size == 0) {
       size += 1;
     } else {
@@ -52,82 +52,79 @@ public class SuperArray {
     return "" + data[index];
   }
 
-}
-
 /////////////////// Part 2 of SuperArray ///////////////////
 
-  // public void clear() {
-  //   int count = 0;
-  //   while (count < size()) {
-  //     add(null);
-  //     count++;
-  //   }
-  // }
+  public void clear() {
+    for (int i = 0; i < size; i++) {
+      set(i, null);
+    }
+  }
 
-  // public void add(int index, String element) {
-  //   if (size() == size) resize();
-  //   String tempString = "" + data[index];
-  //   set(index, element);
-  //   for (int i = index + 1; i < size() - 1; i++) {
-  //     set(i, tempString);
-  //     tempString = "" + data[i+1];
-  //   }
-  // }
+  public boolean isEmpty() {
+    return size() == 0;
+  }
 
-  // public String remove(int index) {
-  //   String removedString = "" + data[index];
-  //   String tempString = "";
-  //   for (int i = index; i < size() - 1; i++) {
-  //     tempString = "" + data[i+1];
-  //     set(i, tempString);
-  //   }
-  //   set(size()-1, null);
-  //   return removedString;
-  // }
+  public String toString() {
+    String arrToString = "[";
+    for (int i = 0; i < size - 1; i++) {
+      if (i < size() -1) {
+        arrToString += data[i] + ", ";
+      }
+      else if ((i == size() - 1)) {
+        arrToString += data[i];
+      } else {
+      }
+    }
+    arrToString += "]";
+    return arrToString;
+  }
 
-  // public int indexOf(String s) {
-  //   int stringLength = s.length();
-  //   int firstIndex = -1;
-  //   for (int i = 0; i < size() - 1; i++) {
-  //     if (data[i].equals(s)) {
-  //       firstIndex = i;
-  //     }
-  //   }
-  //   return firstIndex;
-  // }
+  public boolean contains(String s) {
+    for (int i = 0; i < size() - 1; i++) {
+      if (data[i].equals(s)) return true;
+    }
+    return false;
+  }
 
-  // public String[] toArray() {
-  //   String[] safeArray = data;
-  //   return safeArray;
-  // }
+  public SuperArray(int initialCapacity) {
+    size = initialCapacity;
+    data = new String [size];
+  }
 
-  // public SuperArray(int initialCapacity) {
-  //   size = initialCapacity;
-  //   this.data = new String [size];
-  // }
+  public void add(int index, String element) {
+    if (size() == size) resize();
+    String tempString = "" + data[index];
+    set(index, element);
+    for (int i = index + 1; i < size() - 1; i++) {
+      set(i, tempString);
+      tempString = "" + data[i+1];
+    }
+  }
 
-  // public boolean isEmpty() {
-  //   return size() == 0;
-  // }
+  public String remove(int index) {
+    String removedString = "" + data[index];
+    String tempString = "";
+    for (int i = index; i < size() - 1; i++) {
+      tempString = "" + data[i+1];
+      set(i, tempString);
+    }
+    set(size()-1, null);
+    return removedString;
+  }
 
-  // public String toString() {
-  //   String arrToString = "[";
-  //   for (int i = 0; i < size - 1; i++) {
-  //     if (i < size() -1) {
-  //       arrToString += data[i] + ", ";
-  //     }
-  //     else if ((i == size() - 1)) {
-  //       arrToString += data[i];
-  //     } else {
-  //     }
-  //   }
-  //   arrToString += "]";
-  //   return arrToString;
-  // }
+  public int indexOf(String s) {
+    int firstIndex = -1;
+    for (int i = 0; i < size() - 1; i++) {
+      if (data[i].equals(s)) {
+        firstIndex = i;
+      }
+    }
+    return firstIndex;
+  }
 
-  // public boolean contains(String s) {
-  //   for (int i = 0; i < size() - 1; i++) {
-  //     if (data[i].equals(s)) return true;
-  //   }
-  //   return false;
-  // }
+  public String[] toArray() {
+    String[] safeArray = data;
+    return safeArray;
+  }
+
+}
