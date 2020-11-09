b2f6ba59735074626d8e39bb66c375baa9eaa529
Jonathan Wu <jwu20@stuy.edu>
2020-11-09 16:17:27 -0500

    added findOverlap method

--- Demo.java
+++ Demo.java
@@ -7,6 +7,12 @@ public class Demo {
     System.out.println(words);
     removeDuplicates(words);
     System.out.println(words);
+
+    SuperArray wordsTwo = new SuperArray();
+    wordsTwo.add("kani");   wordsTwo.add("uni");     wordsTwo.add("ebi");     wordsTwo.add("una");
+    wordsTwo.add("una");    wordsTwo.add("ebi");     wordsTwo.add("kani");    wordsTwo.add("una");
+    wordsTwo.add("una");    wordsTwo.add("ebi");     wordsTwo.add("toro");
+    System.out.println(findOverlap(words, wordsTwo));
   }
 
 /////////////////// Part 3 of SuperArray ///////////////////
@@ -18,8 +24,23 @@ public class Demo {
       for (int j = i; j < (s.toArray().length); j++) {
         if ((s.toArray()[j].equals(toRemove)) && (j > indexed)) {
           s.remove(j);
+          j--;
         }
       }
     }
+  }
+
+  public static SuperArray findOverlap(SuperArray a, SuperArray b) {
+    SuperArray overlaps = new SuperArray();
+    for (int i = 0; i < (a.toArray().length); i++) {
+      String toOverlap = a.toArray()[i];
+      for (int j = 0; j < (b.toArray().length); j++) {
+        if (b.toArray()[j].equals(toOverlap)) {
+          overlaps.add(toOverlap);
+        }
+      }
+    }
+    removeDuplicates(overlaps);
+    return overlaps;
   }
 }
