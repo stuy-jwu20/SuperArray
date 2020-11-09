9d8940769466021391a7e129e90d6b11aee7fa49
Jonathan Wu <jwu20@stuy.edu>
2020-11-09 17:09:30 -0500

    Push to make sure everything is up to date

--- Tester.java
+++ Tester.java
@@ -1,26 +1,14 @@
 public class Tester {
   public static void main(String[] args) {
     System.out.println("SuperArray Mini-Tester");
-    SuperArray words = new SuperArray(0);
-    words.add("kani");
-    words.add("uni");
-    words.add("ebi");
-    for(int i = 0; i < words.size(); i++){
-      System.out.println( words.get(i) );
-    }
-    //words.clear();
-    System.out.println(words.toString());
-    System.out.println(words.isEmpty());
-    System.out.println(words.contains("kani"));
-    System.out.println(words.contains("kaniUNI"));
-    words.add(1, "addedString");
-    System.out.println(words.toString());
-    System.out.println(words.remove(0));
-    System.out.println(words.toString());
-    System.out.println(words.indexOf("addedString"));
-    SuperArray newArray = new SuperArray();
-    newArray.add("Ah");
-    newArray.toArray();
-    System.out.println(newArray.toString());
+    SuperArray words = new SuperArray();
+    words.add("kani");   words.add("uni");     words.add("ebi");     words.add("una");
+    words.add("una");    words.add("ebi");     words.add("kani");    words.add("una");
+    words.add("una");    words.add("ebi");     words.add("toro");
+    SuperArray otherArray = new SuperArray();
+    otherArray.add("kani");   otherArray.add("uni");     otherArray.add("ebi");     otherArray.add("una");
+    otherArray.add("una");    otherArray.add("ebi");     otherArray.add("kani");    otherArray.add("una");
+    otherArray.add("una");    otherArray.add("ebi");     otherArray.add("toro");
+    System.out.println(words.equals(otherArray));
   }
 }
